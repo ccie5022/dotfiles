4920b21bf0ae7761ecfae5d00542e85fae27b50b
Bill Carter - @ccie5022 <billyc5022@gmail.com>
2016-01-11 13:15:40 -0600

    First Commit

---
+++ apt-install/apt-install.sh
@@ -0,0 +1,41 @@
+# installs base packages \
+# the utter bare minimal shit \
+base() { \
+	sudo apt-get update && apt-get -y upgrade
+	sudo apt-get install -y \
+        bridge-utils \
+        curl \
+        curlftpfs \
+        deborphan \
+        fakeroot \
+        fonts-hack-ttf \
+        fortune-mod \
+        gedit-developer-plugins \
+        git \
+        git-hub \
+        git-man \
+        gnome-tweak-tool \
+        htop \
+        indent \
+        light-themes \
+        linuxinfo \
+        openssl \
+        open-vm-tools \
+        open-vm-tools-desktop \
+        nautilus-dropbox \
+        ntp \
+        ngrep \
+        silversearcher-ag \
+        ssh \
+        openssh-sftp-server \
+        synaptic \
+        telnet \
+        tree \
+        whois \
+        idle \
+        idle-python2.7 \
+        python-setuptools \
+        python-software-properties \
+        python-pip \
+        python-pbr \
+        python-crypto
---
+++ python/python.sh
@@ -0,0 +1,9 @@
+Python PIP Install
+pep8 \ 
+pylint \ 
+python-ecdsa \ 
+python-yaml \ 
+python-pycurl \ 
+python-jinja2 \ 
+paramiko \ 
+netmiko \ 
