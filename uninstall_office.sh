803db4bf7cead4faa39197ffda3b528271c5ca49
Bill Carter - @ccie5022 <billyc5022@gmail.com>
2016-01-12 10:58:50 -0600

    uninstall libreoffice

---
+++ uninstall_office.sh
@@ -0,0 +1,4 @@
+apt-get remove --purge libreoffice*
+apt-get clean
+apt-get autoremove
+apt-get remove fonts-opensymbol libreoffice libreoffice-\* openoffice.org-dtd-officedocument1.0 python\*-uno uno-libs3-\* ure ure-dbg
