16ce7a34497e73e5777ba62fe26a924885bd81dd
ccie5022 <billyc5022@gmail.com>
2016-03-09 08:40:32 -0600

    remove sudo -su

--- init/swap.sh
+++ init/swap.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Fixing Swappines - Version bump 2
 
-sudo su
 echo "Fixing Swappines"
 echo "vm.swappiness=10" >> /etc/sysctl.conf
 echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
